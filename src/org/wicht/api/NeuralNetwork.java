0e533f9f3c4dd431cad5b78de153f56c4c4f89f9
Baptiste Wicht <baptiste.wicht@gmail.com>
2013-04-22 16:09:12 +0200

    Minor cleanup

--- src/org/wicht/api/NeuralNetwork.java
+++ src/org/wicht/api/NeuralNetwork.java
@@ -18,53 +18,38 @@ public class NeuralNetwork {
         this.function = function;
     }
 
-    public void build(int i, int h, int o){
+    public void build(int i, int... sizes) {
         //1. Build the intput layer
 
         layers.add(new ArrayList<AbstractNeuron>(i));
 
-        for(int n = 0; n < i; ++n){
+        for (int n = 0; n < i; ++n) {
             getInputLayer().add(new InputNeuron());
         }
 
-        //2. Build the hidden layer
+        //2. Build the hidden and output layers
 
-        layers.add(new ArrayList<AbstractNeuron>(h));
+        for (int k = 0; k < sizes.length; ++k) {
+            int size = sizes[k];
+            List<AbstractNeuron> layer = new ArrayList<>(size);
 
-        for(int n = 0; n < h; ++n){
-            NormalNeuron neuron = new NormalNeuron();
-            neuron.setFunction(function);
+            for (int n = 0; n < size; ++n) {
+                NormalNeuron neuron = new NormalNeuron();
+                neuron.setFunction(function);
 
-            for(AbstractNeuron inputNeuron : getInputLayer()){
-                Synapse synapse = new Synapse(inputNeuron, neuron);
+                for (AbstractNeuron inputNeuron : layers.get(k)) {
+                    Synapse synapse = new Synapse(inputNeuron, neuron);
 
-                inputNeuron.addOutputSynapse(synapse);
-                neuron.addInputSynapse(synapse);
+                    inputNeuron.addOutputSynapse(synapse);
+                    neuron.addInputSynapse(synapse);
+                }
+
+                neuron.setBiasConnection(new Synapse(bias, neuron));
+
+                layer.add(neuron);
             }
 
-            neuron.setBiasConnection(new Synapse(bias, neuron));
-
-            layers.get(1).add(neuron);
-        }
-
-        //Build the output layer
-
-        layers.add(new ArrayList<AbstractNeuron>(o));
-
-        for(int n = 0; n < o; ++n){
-            NormalNeuron neuron = new NormalNeuron();
-            neuron.setFunction(function);
-
-            for(AbstractNeuron inputNeuron : layers.get(1)){
-                Synapse synapse = new Synapse(inputNeuron, neuron);
-
-                inputNeuron.addOutputSynapse(synapse);
-                neuron.addInputSynapse(synapse);
-            }
-
-            neuron.setBiasConnection(new Synapse(bias, neuron));
-
-            getOutputLayer().add(neuron);
+            layers.add(layer);
         }
     }
 
@@ -76,19 +61,19 @@ public class NeuralNetwork {
         return layers.get(layers.size() - 1);
     }
 
-    public void train(List<List<Double>> inputs, List<List<Double>> expected, int maxIterations, double maxError){
+    public void train(List<List<Double>> inputs, List<List<Double>> expected, int maxIterations, double maxError) {
         System.out.println("Start training");
 
         int iteration;
 
         double error = 1;
-        for(iteration = 0; iteration < maxIterations && error > maxError; ++iteration){
+        for (iteration = 0; iteration < maxIterations && error > maxError; ++iteration) {
             error = 0;
 
-            for(int p = 0; p < inputs.size(); ++p){
+            for (int p = 0; p < inputs.size(); ++p) {
                 List<Double> output = activate(inputs.get(p));
 
-                for(int j = 0; j < expected.get(p).size(); ++j){
+                for (int j = 0; j < expected.get(p).size(); ++j) {
                     error += Math.pow(output.get(j) - expected.get(p).get(j), 2);
                 }
 
@@ -96,7 +81,7 @@ public class NeuralNetwork {
             }
         }
 
-        if(iteration == maxIterations){
+        if (iteration == maxIterations) {
             System.out.println("Failed to reach precision");
         }
 
@@ -106,25 +91,25 @@ public class NeuralNetwork {
     }
 
     private void backPropagate(List<Double> expected) {
-        for(int i = 0; i < expected.size(); ++i){
+        for (int i = 0; i < expected.size(); ++i) {
             double d = expected.get(i);
 
-            if(d < 0){
+            if (d < 0) {
                 expected.set(i, 0 + epsilon);
-            } else if(d > 1){
+            } else if (d > 1) {
                 expected.set(i, 1 - epsilon);
             }
         }
 
-        for(int i = 0; i < expected.size(); ++i){
+        for (int i = 0; i < expected.size(); ++i) {
             Neuron neuron = getOutputLayer().get(i);
 
-            for(Synapse synapse : neuron.getInConnections()){
+            for (Synapse synapse : neuron.getInConnections()) {
                 double ak = neuron.getOutput();
                 double ai = synapse.getInputNeuron().getOutput();
                 double desired = expected.get(i);
 
-                double partialDerivative = -ak * (1 -ak) * ai * (desired - ak);
+                double partialDerivative = -ak * (1 - ak) * ai * (desired - ak);
                 double delta = -learningRate * partialDerivative;
                 double newWeight = synapse.getWeight() + delta;
 
@@ -133,21 +118,21 @@ public class NeuralNetwork {
             }
         }
 
-        for(Neuron neuron : layers.get(1)){
-            for(Synapse inSynapse : neuron.getInConnections()){
+        for (Neuron neuron : layers.get(1)) {
+            for (Synapse inSynapse : neuron.getInConnections()) {
                 double aj = neuron.getOutput();
                 double ai = inSynapse.getInputNeuron().getOutput();
 
                 double sumKoutputs = 0;
                 int j = 0;
 
-                for(Synapse outSynapse : neuron.getOutConnections()){
+                for (Synapse outSynapse : neuron.getOutConnections()) {
                     Neuron out_neu = outSynapse.getOutputNeuron();
 
                     double wjk = outSynapse.getWeight();
                     double desired = expected.get(j++);
                     double ak = out_neu.getOutput();
-                    sumKoutputs += -(desired -ak) * ak * (1 - ak) * wjk;
+                    sumKoutputs += -(desired - ak) * ak * (1 - ak) * wjk;
                 }
 
                 double partialDerivative = aj * (1 - aj) * ai * sumKoutputs;
@@ -160,21 +145,21 @@ public class NeuralNetwork {
         }
     }
 
-    public List<Double> activate(List<Double> inputs){
-        for(int i = 0; i < getInputLayer().size(); ++i){
+    public List<Double> activate(List<Double> inputs) {
+        for (int i = 0; i < getInputLayer().size(); ++i) {
             getInputLayer().get(i).setOutput(inputs.get(i));
         }
 
-        for(List<AbstractNeuron> layer : layers){
-            for(Neuron neuron : layer){
+        for (List<AbstractNeuron> layer : layers) {
+            for (Neuron neuron : layer) {
                 neuron.activate();
             }
         }
 
         List<Double> results = new ArrayList<>();
 
-        for(Neuron neuron : getOutputLayer()){
-           results.add(neuron.getOutput());
+        for (Neuron neuron : getOutputLayer()) {
+            results.add(neuron.getOutput());
         }
 
         return results;
